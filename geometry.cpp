379dc446ba4b064dfd29b1ec97e57f2066526313
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2019-10-31 16:05:06 +0400

    fixed type in vector

--- geometry.cpp
+++ geometry.cpp
@@ -7,7 +7,7 @@ struct vec {
   vec operator-(const vec &oth) const { return vec(x - oth.x, y - oth.y); }
   vec operator*(const T k) const { return vec(x*k, y*k); }
   T operator*(const vec &oth) const { return x*oth.x + y*oth.y; }
-  T operator%(const vec &ith) const { return x*oth.y - y*oth.x; }
+  T operator%(const vec &oth) const { return x*oth.y - y*oth.x; }
   T len2() const { return x*x + y*y; }
   long double len() const { return sqrt(x*x + y*y); }
   vec norm() const { long double length = sqrt(x*x + y*y); return vec(x / length, y / length); }
